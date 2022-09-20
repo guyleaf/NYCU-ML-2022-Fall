ff3640fdb34c4d57c2b60470aa694fc4cb756d55
guyleaf <ychhua1@gmail.com>
2022-09-20 18:36:56 +0800

    Add main.cpp

---
+++ hw1/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+#include <stdexcept>
+
+std::vector<std::array<int, 2>> getData(std::string path) {
+    return std::vector<std::array<int, 2>>{};
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        throw std::invalid_argument("The arguments should be textfile path, polynomial bases, lambda (LSE).");
+    }
+    return 0;
+}
