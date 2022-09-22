32cbcba2ea52e84a3644923c4357844d9fce6e6c
guyleaf <leaf.ying.work@gmail.com>
2022-09-23 00:52:05 +0800

    Finish loading data

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,15 +1,32 @@
 ﻿#include <iostream>
-#include <vector>
-#include <array>
+
+#include <string>
 #include <stdexcept>
 
-std::vector<std::array<int, 2>> getData(std::string path) {
-    return std::vector<std::array<int, 2>>{};
-}
+#include "regression.hpp"
+#include "utils.hpp"
 
-int main(int argc, char* argv[]) {
-    if (argc < 4) {
+
+int main(int argc, char *argv[])
+{
+    if (argc < 4)
+    {
         throw std::invalid_argument("The arguments should be textfile path, polynomial bases, lambda (LSE).");
     }
+    if (!utils::isNumber(argv[2]))
+    {
+        throw std::invalid_argument("The arguments polynomial bases must be a number.");
+    }
+    if (!utils::isNumber(argv[3]))
+    {
+        throw std::invalid_argument("The arguments lambda (LSE) must be a number.");
+    }
+
+    std::string filePath(argv[1]);
+    int numberOfbases = std::stoi(argv[2]);
+    double lambda = std::stof(argv[3]);
+
+    auto points = utils::getData(filePath);
+
     return 0;
 }
---
+++ hw1/regression.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "utils.hpp"
+
+namespace regression
+{
+    utils::Vectord fitPointsByLSE(utils::Matrix2d points, int numberOfBases, double lambda)
+    {
+        return {};
+    }
+
+    utils::Vectord fitPointsByNewtonMethod(utils::Matrix2d points, int numberOfBases)
+    {
+        return {};
+    }
+} // namespace regression
